f6c44275e32d1af97dd8fb8feeffe5195ea35e94
Johnny Ray Austin <johnny@johnnyray.me>
2018-03-10 11:14:59 -0500

    Update index.js

--- workshop1/exercise-1-functional-programming/solution/index.js
+++ workshop1/exercise-1-functional-programming/solution/index.js
@@ -1,10 +1,16 @@
 
+// require the file system module so we can read files
 const fs = require('fs')
+// read the json file, and parse it so that we can use it like
+// a regular JavaScript object
 const all_people = JSON.parse(fs.readFileSync('../../people.json'))
 const people = all_people
+  // filter out anyone who is not active
   .filter(p => p.isActive === true)
+  // give everyone a $1,000,000
   .map(p => {
     p.balance = '$1,000,000'
     return p
   })
+  // print the name and balance of everyone left in the array
   .forEach(p => console.log(`${p.name} has ${p.balance}`))
