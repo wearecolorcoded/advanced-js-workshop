81703226ebee7b3481a7313b6c49d315a9274af6
recursivefunk <johnny@johnnyray.me>
2018-03-10 12:34:03 -0500

    adding lesson tests

--- workshop2/testing/app.js
+++ workshop2/testing/app.js
@@ -1,2 +1,18 @@
 
 exports.add_numbers = (num1, num2) => num1 + num2
+
+exports.add_numbers_async = (num1, num2, callback) => {
+  const sum = num1 + num2
+  setTimeout(() => callback(null, sum), 0)
+}
+
+exports.add_numbers_promisly = (num1, num2) => {
+  return new Promise((resolve, reject) => {
+    const sum = num1 + num2
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      reject(new Error('Either num1 or num2 is not a valid number!'))
+    } else {
+      resolve(sum)
+    }
+  })
+}
--- workshop2/testing/app_test.js
+++ workshop2/testing/app_test.js
@@ -1,1 +1,54 @@
 
+const test = require('tape')
+const app = require('./app')
+
+test.only('adds numbers using async await', async t => {
+  const expected_sum = 3
+  let sum
+  try {
+    sum = await app.add_numbers_promisly('1', 2)
+  } catch(err) {
+    console.error('oops!')
+    t.pass()
+  }
+  t.end()
+})
+
+test('adds numbers promisly', t => {
+  const expected_sum = 3
+  app.add_numbers_promisly(1, 2)
+    .then(result => {
+      t.is(result, expected_sum)
+      t.end()
+    })
+    .catch(err => {
+      console.error(err)
+      t.fail()
+    })
+})
+
+test('adds numbers asyncly', t => {
+  const expected_sum = 3
+  app.add_numbers_async(1, 2, (err, result) => {
+    t.is(typeof expected_sum, 'number')
+    t.is(result, expected_sum, 'sum is equal to 3')
+    t.end()
+  })
+})
+
+test('ensure add_numbers adds numbers', t => {
+  const sum = app.add_numbers(1, 2)
+  t.is(typeof sum, 'number')
+  t.is(3, sum, 'sum is equal to 3')
+  t.end()
+})
+
+test('ensure we get garbage', t => {
+  const sum = app.add_numbers('1', 2)
+  if (typeof sum !== 'number') {
+    t.pass()
+  } else {
+    t.fail()
+  }
+  t.end()
+})
